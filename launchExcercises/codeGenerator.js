77b9c3104379e5ccb0c67bb6f9f4f488ce43d716
Niko Kapfer-Rupp <nikolaus.kapfer.rupp@gmail.com>
2023-11-17 09:59:32 +0000

    add launchExcercises

---
+++ launchExcercises/codeGenerator.js
@@ -0,0 +1,15 @@
+let code;
+
+let randomNumber = () => Math.floor(Math.random() * 10);
+
+code = randomNumber() + (randomNumber() * 10) + (randomNumber() * 100) +  (randomNumber() * 1000);
+
+if(code < 1000) {
+  let str1 = '0';
+  code = str1.concat(String(code));
+} else {
+  code = String(code);
+}
+console.log(code);
+console.log(typeof code);
+
---
+++ launchExcercises/example.js
@@ -0,0 +1,3 @@
+console.log('Hello, world!');
+let test = 'xyzzy'.toUpperCase();
+console.log(test);
