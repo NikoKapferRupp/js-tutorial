9b0dacca98a256e515ed96ec02960a0cb7181f1e
Niko Kapfer-Rupp <nikolaus.kapfer.rupp@gmail.com>
2023-12-05 22:24:26 +0000

    Added Code for tictactoe game

---
+++ launch_excercises/for_fun/tic_tac_toe.js
@@ -0,0 +1,37 @@
+let openSpaces = [ " ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+function fieldBuilder() {
+    console.log("___________________");
+    console.log("|     |     |     |");
+    console.log("|  " + openSpaces[0] + "  |  " + openSpaces[1] + "  |  " + openSpaces[2] + "  |");
+    console.log("|0____|1____|2____|");
+    
+    
+    /*if (openSpaces[0] === 0, openSpaces[1] === 1, openSpaces[2] === 2) {
+        console.log("___________________\n|     |     |     |\n|     |     |     |\n|1____|2____|3____|")
+    } else if (openSpaces[0] === 0, openSpaces[1] === 1, openSpaces[2] !== 2) {
+        if(openSpaces[]) {
+            console.log();
+        }
+        
+        
+    } else if (openSpaces[0] === 0, openSpaces[1] !== 1, openSpaces[2] !== 2) {
+        
+    } else if (openSpaces[0] !== 0, openSpaces[1] !== 1, openSpaces[2] !== 2) {
+        
+    } else if (openSpaces[0] !== 0, openSpaces[1] !== 1, openSpaces[2] === 2) {
+        
+    } else if (openSpaces[0] !== 0, openSpaces[1] === 1, openSpaces[2] === 2) {
+        
+    } else if (openSpaces[0] === 0, openSpaces[1] !== 1, openSpaces[2] === 2) {
+        
+    } else if (openSpaces[0] !== 0, openSpaces[1] === 1, openSpaces[2] !== 2) {
+        
+    }
+        
+
+fieldBuilder(); */
+};
+fieldBuilder();
+openSpaces[0] = 'X';
+fieldBuilder();
